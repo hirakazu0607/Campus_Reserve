d75bafe710a861266a0b628a60bc4c510184a1e8
hirakazu_desktop <surakazu0607@outlook.jp>
2025-12-14 02:17:07 +0900

    CI test fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
   factory :user do
     # 名前
     sequence(:name) { |n| "テストユーザー#{n}" }
-    
+
     # 学籍番号・社員番号（重複しないようにsequenceを使用）
     sequence(:student_or_staff_number) { |n| 100000 + n } # 100001, 100002, 100003...
 
