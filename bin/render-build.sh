557e4a7d5ba827a70d62265dee1c777102195d4f
hirakazu_desktop <surakazu0607@outlook.jp>
2025-12-15 23:08:25 +0900

    Fix Render build: exclude development/test gems

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
-bundle install
+# Install dependencies excluding development and test gems
+bundle install --without development test
+
+# Precompile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Run database migrations
 bundle exec rails db:migrate
